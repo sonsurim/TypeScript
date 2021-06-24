9dc9ab8677ac08692faaf4b36304b2a2ba410c88
sonsurim <surim014@naver.com>
2021-06-24 22:42:13 +0900

    [Update] 5_operator.ts

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -32,8 +32,27 @@ interface Person {
   age: number;
 }
 
-function askSomeone(someone: Developer | Person) {
+// function askSomeone(someone: Developer | Person) {
+//   someone.name
+//   someone.skill
+//   someone.age
+// }
+
+// askSomeone()
+// askSomeone({ name: '디벨로퍼', skill: '웹 개발'});
+// askSomeone({ name: '수림', age: 100 });
+// askSomeone이라는 함수의 인자로 Developer의 데이터 || Perosn의 데이터를 할당 가능
+
+function askSomeone(someone: Developer & Person) {
   someone.name
   someone.skill
   someone.age
 }
+
+askSomeone({ name: '디벨로퍼', skill: '웹 개발'});
+askSomeone({ name: '수림', age: 100 });
+askSomeone({name: '수림디벨로퍼', age:100, skill: '웹 개발'});
+// Developer와 Person의 속성들을 모두 합친 하나의 타입만 할당 가능
+
+let rim: string | number | boolean;
+let son: string & number & boolean;
