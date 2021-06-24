b20da2ce7e79779ece91409d191231006a3b34e1
sonsurim <surim014@naver.com>
2021-06-25 00:28:33 +0900

    [Update] 7_class.ts

---
+++ class-note/7_class.ts
@@ -0,0 +1,11 @@
+class Person {
+  // 멤버 변수
+  private name: string;
+  public age: number;
+  readonly log: string;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
