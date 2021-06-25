dc4219d70243a8ae93fe15ab4169267a6c0a2bd7
sonsurim <surim014@naver.com>
2021-06-26 02:16:50 +0900

    [Update] dropdown-generics.ts

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,36 +1,33 @@
+// AS-IS
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
+
+// TO-BE
 interface DropdownItem<T> {
   value: T;
   selected: boolean;
 }
 
-// interface Email {
-//   value: string;
-//   selected: boolean;
-// }
-
 const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+  { value: 'google.com', selected: false },
+  { value: 'hanmail.net', selected: false }
 ];
 
-// interface ProductNumber {
-//   value: number;
-//   selected: boolean;
-// }
-
-// interface TrueFalse {
-//   value: boolean;
-//   selected: boolean;
-// }
-
-const numberOfProducts: DropdownItem<number>[] = [
+const numberOfProducts: DropdownItem<number>[]= [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
-];
+]
 
-function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
+function createDropdownItem<T>(item: DropdownItem<T> ) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -38,13 +35,12 @@ function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
   return option;
 }
 
-// NOTE: 이메일 드롭 다운 아이템 추가
-emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+emails.forEach(function (email: DropdownItem<string>) {
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
 
-numberOfProducts.forEach(function (product) {
-  const item = createDropdownItem(product);
+numberOfProducts.forEach(function (product: DropdownItem<number>) {
+  const item = createDropdownItem<number>(product);
 });
