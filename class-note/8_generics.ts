dcdec1bc5d2fb606609d2345abdc18474f5b1672
sonsurim <surim014@naver.com>
2021-06-26 02:16:14 +0900

    [Update] 8_generics.ts

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -89,3 +89,59 @@ const obj: Dropdown<string> = { value: 'abc', selected: false };
 
 // Dropdown 인터페이스를 정의하는 데에 제네릭으로 정의를 할 때 타입을 선언하는 시점에
 // 타입을 추가적으로 넘겨서 Dropdown이라는 인터페이스의 타입을 바꿀 수 있음
+
+// 제네릭의 타입 제한
+// - 제네릭을 더 엄격하게 사용할 때 접근하는 방식
+// - 제네릭에 더 많은 옵션들을 사용할 때 접근하는 방식
+
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach( text => console.log(text))
+//   return text;
+// }
+
+// logTextLength<string>(['hi', 'abc']);
+
+// 제네릭의 타입 제한 - 2. 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+// LengthType 인터페이스의 length 속성이 number라는 타입을 가짐
+
+function logTextLength<T extends LengthType>(text: T): T {
+  // text.length;
+  return text;
+}
+// 제네릭으로 받은 타입에 length 속성이 있을거라는 암시
+// length 속성을 가지고 있는 타입만 인자로 받음
+
+logTextLength('a');
+// 문자열은 내부 속성으로 length가 제공
+logTextLength([1]);
+// 배열은 내부 속성으로 length가 제공
+logTextLength({length: 10})
+// length 속성이 있음
+
+// ERROR
+logTextLength({leng: 10});
+// 객체는 내부 속성으로 length가 제공되지 않음 따로 length 속성 정의 필요
+logTextLength(10);
+// 숫자는 length가 내부 속성으로 제공되고 있지 않음
+
+// 제네릭의 타입 제한 - 3. keyof로 제네릭의 타입 제한
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+// ShoppingItem 인터페이스에 정의되어 있는 속성(name,price,stock) 중 하나만 받도록 제한
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+   return itemOption;
+}
+// ShoppingItem 인터페이스에 있는 key 중 한가지가 제네릭이 될 것이라고 명시
+// name, price, stock중 한가지만 인자로 들어올 수 있음
+
+getShoppingItemOption('name')
+getShoppingItemOption('price')
+getShoppingItemOption('stock')
