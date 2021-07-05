eade30bf90b5c0fcd55b1ca9d65c4d083742acb5
sonsurim <surim014@naver.com>
2021-07-05 23:46:37 +0900

    [Update] index.ts

---
+++ project/src/covid/index.ts
@@ -0,0 +1,31 @@
+interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: 0;
+  NewRecovered: 0;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+export interface CovidSummaryResponse {
+  Countries: Country[];
+  Date: string;
+  Global: Global;
+  Message: string;
+}
